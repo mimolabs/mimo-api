528a5c42e1668723b8e7fe42cca88a7a9a11d822
Simon Morley <simon@polkaspots.com>
2018-05-29 14:53:08 +0100

    Sidekiq init

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'production'
+
+require 'sidekiq'
+
+sidekiq_config = YAML.load(ERB.new(File.read(rails_root + '/config/redis_server.yml')).result)
+
+# Sidekiq.configure_server do |config|
+#   config.redis = { :host => "#{sidekiq_config[rails_env]}"}
+# end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :host => "#{sidekiq_config[rails_env]}"}
+end
