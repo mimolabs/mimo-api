ad3c31f4641c9a899be6549ad91ba7624ca933c3
Simon Morley <simon@polkaspots.com>
2018-06-05 11:28:41 +0100

    square logo formatting

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -7,7 +7,9 @@ class LogoUploader < CarrierWave::Uploader::Base
     "uploads"
   end
 
-  process :resize_to_fit => [200, 200]
+  process :efficient_conversion => [200, 200]
+
+  # process :resize_to_fit => [200, 200]
 
   # Create different versions of your uploaded files:
   version :thumb do
@@ -17,12 +19,26 @@ class LogoUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(png)
+    %w(jpg jpeg gif png bmp tif tiff)
   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
     "square-logo.png" if original_filename
+  end
+
+  private
+
+  def efficient_conversion(width, height)
+    manipulate! do |img|
+      img.format("png") do |c|
+        c.fuzz        "3%"
+        c.trim
+        c.resize      "#{width}x#{height}>"
+        c.resize      "#{width}x#{height}<"
+      end
+      img
+    end
   end
 end
