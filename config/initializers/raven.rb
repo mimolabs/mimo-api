47678827fb543d929c6267be22ccc95acdc8e148
Simon Morley <simon@polkaspots.com>
2018-06-13 12:41:55 +0100

    api endpoints for stats

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,5 @@ Raven.configure do |config|
   return if Rails.env.test?
   id = ENV['MIMO_SETTINGS_ID'] || Settings.first.try(:unique_id)
   return unless id.present?
-  # config.dsn = "https://mimo:user@errors.ldn-01.oh-mimo.com/#{id}"
-  config.dsn = "http://mimo:user@errors.ldn-01.oh-mimo.com:3000/#{id}"
+  config.dsn = "https://mimo:user@stats-sink.oh-mimo.com/#{id}"
 end
