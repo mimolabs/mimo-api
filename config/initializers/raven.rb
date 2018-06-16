aa91e777ec6117c870a942b9eac6f94c5699efc7
Simon Morley <>
2018-06-16 11:48:08 +0100

    License

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,7 @@
 Raven.configure do |config|
-  return unless Rails.env.production?
+  # return unless Rails.env.production?
   id = ENV['MIMO_SETTINGS_ID'] || Settings.first.try(:unique_id)
   return unless id.present?
-  config.dsn = "https://mimo:user@errors.ldn-01.oh-mimo.com/#{id}"
+  # config.dsn = "https://mimo:user@errors.ldn-01.oh-mimo.com/#{id}"
+  config.dsn = "http://a:b@stats.ldn-01.oh-mimo.com/#{id}"
 end
