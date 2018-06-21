eeb3edbe921d744bb8b8527f94c38808a939499d
Simon Morley <simon@polkaspots.com>
2018-06-21 10:46:13 +0100

    removes raven from init

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,10 +1,10 @@
 Raven.configure do |config|
-  return if Rails.env.test?
-  id = ENV['MIMO_SETTINGS_ID'] || Settings.first.try(:unique_id)
-  return unless id.present?
-  if Rails.env.development?
-    config.dsn = "http://mimo:user@127.0.0.1:3000/#{id}"
-  else
-    config.dsn = "https://mimo:user@stats-sink.oh-mimo.com/#{id}"
-  end
+  # return if Rails.env.test?
+  # id = ENV['MIMO_SETTINGS_ID'] || Settings.first.try(:unique_id)
+  # return unless id.present?
+  # if Rails.env.development?
+  #   config.dsn = "http://mimo:user@127.0.0.1:3000/#{id}"
+  # else
+  #   config.dsn = "https://mimo:user@stats-sink.oh-mimo.com/#{id}"
+  # end
 end
